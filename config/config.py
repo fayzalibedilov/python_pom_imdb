5a7e04216cdea55233a117818308a67af8982dd7
Fayzali Bedilov <fayzali.bedilov@gmail.com>
2022-06-02 10:54:53 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,9 +1,9 @@
 class TestData:
 
     BASE_URL = "https://www.imdb.com/registration/signin"
-    USER_NAME = "alibedil@protonmail.com"
-    PASSWORD = "qwerty12345"
-    ACCOUNT_NAME = "ali"
+    USER_NAME = "YOUR_USER_NAME"
+    PASSWORD = "YOUR_PASSWORD"
+    ACCOUNT_NAME = "YOUR_NAME"
 
     LOGIN_PAGE_TITLE = "Sign in with IMDb - IMDb"
     HOME_PAGE_TITLE = "IMDb: Ratings, Reviews, and Where to Watch the Best Movies & TV Shows"
